03439773e75b5048b86855450cfb3c03b44a4e83
Ismail Al amir <109903994+IsmailAlamir@users.noreply.github.com>
2023-02-27 20:31:53 +0300

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,120 @@
+def rearrange_odds_and_evens(arr):
+
+    odd,even = [], []
+    for num in arr:
+        if num %2==0 :
+            even.append(num)
+        else :
+            odd.append(num)
+    
+    res=[]
+    if len(odd)>len(even):
+        while even:
+            res.append(odd.pop(0))
+            res.append(even.pop(0))
+        res+=odd
+    else:
+        while odd:
+            res.append(odd.pop(0))
+            res.append(even.pop(0))
+        res+=even
+    return res
+    
+def not_Dublicates(arr):
+    counts={}
+    res=[]
+  
+    for num in arr:
+        if num in counts:
+            counts[num]+=1
+        else:
+            counts[num]=1
+            
+    for num,count in counts.items():
+        if count==1:
+            res.append(num)
+            
+    return res
+
+def charactusrnumve(string):
+    charCounter=0
+    numberCounter=0
+    speicalCounter=0
+    for char in string:
+        if (ord(char)>=65 and ord(char)<90) or(ord(char)>=97 and ord(char)<122):
+            charCounter+=1
+        elif  (ord(char)>=48 and ord(char)<57):
+            numberCounter+=1
+        else:
+            speicalCounter+=1
+    return charCounter,numberCounter,speicalCounter
+
+
+def missing_number(arr):
+    num = 0
+    max_val = max(arr)
+
+    for j in range(max_val):
+        if j not in arr:
+            num = j
+            break
+
+    return num
+
+
+
+def index_of_repeated_integer_index(arr):
+    seen = set()
+    for i in range(len(arr)):
+        if arr[i] in seen:
+            return i
+        seen.add(arr[i])
+    return "no"
+
+    
+def two_sum(arr, target):
+    hashtable = {}
+    arr2 = [0, 0]
+
+    for i in range(len(arr)):
+        if target - arr[i] in hashtable:
+            arr2[0] = hashtable[target - arr[i]]
+            arr2[1] = i
+        else:
+            hashtable[arr[i]] = i
+    
+    return arr2
+
+
+def two_sum(arr, target):
+    sum=0
+    hash={}
+    for num in arr:
+        value=target-num
+        if value in hash:
+            return [value,num]
+        else :
+            hash[num]=True
+    
+def repeated_integers_with(arr):
+    hashtable = {}
+
+    for num in arr:
+        if num in hashtable:
+            hashtable[num] += 1
+        else:
+            hashtable[num] = 1
+
+    for key, value in hashtable.items():
+        print(key, value)
+arr = [1, 2, 3, 3, 3, 4, 4, 4, 4]
+repeated_integers_with(arr)
+
+
+x=4
+y=5
+x+=y
+y=x-y
+x=x-y
+
+print("x=",x , "y=", y)
