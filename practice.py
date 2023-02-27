8b5009ba3dcd5400841c983ef6e0d8266b131824
Ismail Al amir <109903994+IsmailAlamir@users.noreply.github.com>
2023-02-27 23:32:00 +0300

    functions

--- practice.py
+++ practice.py
@@ -23,17 +23,17 @@ def rearrange_odds_and_evens(arr):
 def not_Dublicates(arr):
     counts={}
     res=[]
-  
+
     for num in arr:
         if num in counts:
             counts[num]+=1
         else:
             counts[num]=1
-            
+
     for num,count in counts.items():
         if count==1:
             res.append(num)
-            
+
     return res
 
 def charactusrnumve(string):
@@ -71,6 +71,15 @@ def index_of_repeated_integer_index(arr):
         seen.add(arr[i])
     return "no"
 
+def index_of_repeated_integer_index(arr):
+    seen = set()
+    
+    for i in reversed(range(len(arr))):
+        print(i)
+        if arr[i] in seen:
+            return i
+        seen.add(arr[i])
+    return "no"
     
 def two_sum(arr, target):
     hashtable = {}
@@ -111,10 +120,51 @@ arr = [1, 2, 3, 3, 3, 4, 4, 4, 4]
 repeated_integers_with(arr)
 
 
-x=4
-y=5
-x+=y
-y=x-y
-x=x-y
+# x=4
+# y=5
+# x+=y
+# y=x-y
+# x=x-y
 
-print("x=",x , "y=", y)
+# print("x=",x , "y=", y)
+
+
+def sum2num(arr):
+    pointer1 = arr[0]
+    pointer2 = arr[0]
+    result = []
+    
+    for i in range(1, len(arr)):
+        if arr[i] == pointer1:
+            pointer2 += arr[i]
+        else:
+            result.append(pointer2)
+            pointer1 = arr[i]
+            pointer2 = arr[i]
+    result.append(pointer2)
+    
+    return result
+
+
+
+    
+def isValid( s: str) -> bool:
+    pairdict = {
+        "(":")",
+        "[":"]",
+        "{":"}"
+    }
+    stack = []
+    for char in s:
+        if char in pairdict:
+            stack.append(char)
+        elif not stack or pairdict[stack.pop()] != char:
+            return False
+
+    return not stack
+
+ 
+def pyramid_of_stars(base):
+    for i in reversed(range(base+1)):
+        print ((i)*"*")
+
